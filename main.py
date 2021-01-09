445d47f1b53f632a28e4a46a93ee53ffa29c6cd1
spgcam <sgluhov@gmail.com>
2021-01-09 21:37:05 +0000

    testing images ver 1.0

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import time
 import picamera
 
 camera = picamera.PiCamera()
-camera.resolution(1024, 768)
+camera.resolution = (4056, 3040)
 camera.start_preview()
 # Let camera warm-up
 time.sleep(2)
 
-folder = os.path.abspath('/usr')
+folder = os.getcwd()
 filename = datetime.datetime.now().strftime('%a - %H-%M') + '.jpg'  # https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
+print(folder, filename, sep='\n')
+
 camera.capture(os.path.join(folder, filename))
